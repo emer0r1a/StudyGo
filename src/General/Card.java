3db68242d0eff0f7cc6fc1bbcc22a831b73f4470
EMERALD <reaemerald2006@gmail.com>
2025-11-28 02:31:20 +0800

    Add decks template

---
+++ src/General/Card.java
@@ -0,0 +1,11 @@
+package General;
+
+public class Card {
+    String question;
+    String answer;
+
+    public Card(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+}
---
+++ src/General/Deck.java
@@ -0,0 +1,39 @@
+package General;
+
+import java.util.ArrayList;
+
+public class Deck {
+    String title;
+    int size;
+    int lastAccessed = 0;
+    String color;
+    String subject = null;
+    ArrayList<Card> cards;
+
+    public Deck(String title, int size, int lastAccessed, String color) {
+        this.title = title;
+        this.size = size;
+        this.lastAccessed = lastAccessed;
+        this.color = color;
+    }
+
+    public void setSubject(String subject) {
+
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getLastAccessed() {
+        return lastAccessed;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
