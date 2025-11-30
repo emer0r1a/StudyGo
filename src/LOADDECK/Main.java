621f97fd58847a63c3657ede2250e7ace91a26df
yciadebby <102646464+yciadebby@users.noreply.github.com>
2025-11-30 18:27:14 +0800

    Update Main.java

--- src/LOADDECK/Main.java
+++ src/LOADDECK/Main.java
@@ -2,20 +2,15 @@ package LOADDECK;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
                 try {
-                    new LOADDECK("file.txt");//for test run, can be omitted after compiled
+                    new LOADDECK("test.txt");//for test run, can be omitted after compiled
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 } catch (FontFormatException e) {
